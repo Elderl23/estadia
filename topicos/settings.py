51dadfdf75086498b8b3e614c5fa9fba2e38a59f
vicky <gar_vicky93@hotmail.com>
2015-12-05 12:15:39 -0600

    Vicky static_dirs

--- topicos/settings.py
+++ topicos/settings.py
@@ -98,5 +98,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR.child('topicos').child('static')]
+STATICFILES_DIRS = [
+#BASE_DIR.child('topicos').child('static')
+os.path.join(PROYECT_ROOT, 'static'),
+]
+
+
 
